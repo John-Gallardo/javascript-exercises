2df0d67740b1f5de64749ba0635ef38a6708d92a
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-05 01:26:01 -0600

    Feat: finished exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeTargets) {
+    let newArray = [];
+    array.forEach((element) => {if (!removeTargets.includes(element)) newArray.push(element)});
+    return newArray;
 };
 
 // Do not edit below this line
