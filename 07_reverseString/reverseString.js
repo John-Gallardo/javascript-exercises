294f1a91db653845fc44abaf71aa9aebf7e3dff4
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-05 01:00:09 -0600

    Feat: finished exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Done this way bec. README said to split.
+    // probably would have just iterated through string backwards which should be faster (?)
+    let newString = string.split("")
+        .reverse()
+        .join("");
+    return newString;
 };
 
 // Do not edit below this line
