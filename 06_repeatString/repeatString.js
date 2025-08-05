900217db22c4030f64ca8a4a6a7ff9d1d4b81659
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-05 00:54:44 -0600

    Feat: finished exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTimes) {
+    if (repeatTimes < 0)
+        return "ERROR";
+    let newString = "";
+    for (let i = 0; i < repeatTimes; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
