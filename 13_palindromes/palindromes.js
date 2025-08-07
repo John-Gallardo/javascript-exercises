b199442f6f0ab9370005202caba138eb643c7116
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-07 17:26:50 -0600

    Feat: finished exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();  // make case insensitive
+    string = string.replace(/[^\w\s]/gi, "");  // remove all punctuation
+    string = string.replaceAll(" ", "")  // remove all space
 
+    let reversedString = string.split('').reverse().join('');
+    if (reversedString == string)
+        return true;
+    return false;
 };
 
 // Do not edit below this line
