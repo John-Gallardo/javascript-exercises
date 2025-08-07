9eda9f0fd68d1d664ef59af044982153e81ef72b
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-07 17:38:13 -0600

    Feat: finished exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0)
+        return "OOPS";
+    else if (number == 0)
+        return 0;
+    else if (number === 1 || number === 2)
+        return 1;
 
+    result = 1, fib_1 = 1, fib_2 = 1;  // fib_1 = fib-1, fib_2 = fib-2
+    // i = 3 because case of fib(1) & fib(2) already dealt with
+    for (let i = 3; i <= number; i++) {
+        fib_2 = fib_1;
+        fib_1 = result;
+        result = fib_1 + fib_2;
+    }
+    return result;
 };
 
 // Do not edit below this line
