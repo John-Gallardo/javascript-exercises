571b9a8838d22900c26cb29870c81035abc857b4
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-07 17:55:01 -0600

    Feat: finisehd exercise 16 :

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = null, oldestIndex = null, currentYear = new Date().getFullYear(), age = null;
+    for (person of people) {
+        if ("yearOfDeath" in person)
+            age = person.yearOfDeath - person.yearOfBirth;
+        else 
+            age = currentYear - person.yearOfBirth;
+
+        if (age > oldest) {
+            oldest = age;
+            oldestIndex = people.indexOf(person);
+        }
+    }
+    return people[oldestIndex];
 
 };
 
